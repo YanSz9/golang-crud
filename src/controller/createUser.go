6cc2555e2b2c4398fb19d0e95d8acdfbd3e32b4e
YanSz9 <yansouza009@gmail.com>
2023-11-15 14:59:12 -0300

    feat: setting up routes

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/YanSz9/golang-crud/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadResquestError("Error on call route")
+	c.JSON(err.Code, err)
+}
