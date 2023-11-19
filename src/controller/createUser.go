70318e9a84eb9683b7315abee61de67d60c22c06
YanSz9 <yansouza009@gmail.com>
2023-11-19 16:10:34 -0300

    feat: creating request and response viewmodel

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,22 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/YanSz9/golang-crud/src/configuration/rest_err"
+	"github.com/YanSz9/golang-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadResquestError("Error on call route")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadResquestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
