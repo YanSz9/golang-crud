fa1cb26cb397400fb9a5b17643a35973eb8de697
YanSz9 <yansouza009@gmail.com>
2023-11-19 17:00:51 -0300

    feat: adding custom errors

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"github.com/YanSz9/golang-crud/src/configuration/rest_err"
+	"github.com/YanSz9/golang-crud/src/configuration/validation"
 	"github.com/YanSz9/golang-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +12,9 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadResquestError(
-			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		errRest := validation.ValidateUserError(err)
 
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 	fmt.Println(userRequest)
