076275ac5b4df3813bedf789b98bd0e3fe9113d4
YanSz9 <yansouza009@gmail.com>
2023-11-20 23:31:06 -0300

    feat: creating api logs

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,21 +1,40 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/YanSz9/golang-crud/src/configuration/logger"
 	"github.com/YanSz9/golang-crud/src/configuration/validation"
 	"github.com/YanSz9/golang-crud/src/controller/model/request"
+	"github.com/YanSz9/golang-crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
-	fmt.Println(userRequest)
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created sucessfully",
+		zap.String("journey", "createUser"))
+
+	c.JSON(http.StatusOK, response)
 }
