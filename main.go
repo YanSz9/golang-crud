3f913c3de626d75ceed66edb5b01af18d35e8c65
YanSz9 <yansouza009@gmail.com>
2023-11-23 00:19:29 -0300

    chore: database config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/YanSz9/golang-crud/src/configuration/database/mongodb"
 	"github.com/YanSz9/golang-crud/src/configuration/logger"
 	"github.com/YanSz9/golang-crud/src/controller"
 	"github.com/YanSz9/golang-crud/src/controller/routes"
@@ -17,6 +18,8 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	mongodb.InitConnection()
 
 	service := service.NewUserDomainService()
 	userController := controller.NewUserControllerInterface(service)
